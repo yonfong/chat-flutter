742cda895d6ec3176a86ea404335965c90d85f37
yonfeng <yonfeng.zhang@gmail.com>
2018-11-08 19:51:20 +0800

    列表数据展示

--- chatflutter/lib/main.dart
+++ chatflutter/lib/main.dart
@@ -19,13 +19,33 @@ class ChatScreen extends StatefulWidget {
 
 class ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
+  final _message = <ChatMessage>[];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Friendlychat'),
       ),
-      body: _buildTextComposer(),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8),
+              reverse: true,
+              itemBuilder: (_, int index) => _message[index],
+              itemCount: _message.length,
+            ),
+          ),
+          Divider(height: 1),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          )
+        ],
+      ),
     );
   }
 
@@ -62,8 +82,49 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = ChatMessage(
+      text: text,
+    );
+    setState(() {
+       _message.insert(0, message);   
+    });
   }
 
 }
 
+const String _name = 'yonfong';
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                _name,
+                style: Theme.of(context).textTheme.subhead
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
 
