6bfa188dc58692e33a8351787c0a71815cebe009
yongfeng <yonfeng.zhang@gmail.com>
2018-11-08 13:20:25 +0800

    add textComposer

--- chatflutter/lib/main.dart
+++ chatflutter/lib/main.dart
@@ -12,16 +12,58 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Friendlychat'),
       ),
+      body: _buildTextComposer(),
     );
   }
-}
 
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed(
+                  hintText: 'send a message'
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            )
+          ],
+        )
+      ),
+    );
+    
+  }
+
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+}
 
 
